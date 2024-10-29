e8aeacd472050025ef928a90ba788c2fd8945f1e
Jonas van den Berg <jonas@vandenbe.com>
2024-10-29 23:42:55 +0100

    Fix injection script that hooks into audio elements

---
+++ public/inject.js
@@ -0,0 +1,18 @@
+// Appends any audio elements that are being played or paused to the DOM
+// so that they can be found and used by the content script.
+// This hook needs to be executed within the web page, not a content script,
+// otherwise overwritin the Audio prototype has no effect.
+
+(function hookFutureAudioElements() {
+  ['play', 'pause'].forEach(method => {
+    const original = Audio.prototype[method];
+    Audio.prototype[method] = function () {
+      console.log(method);
+      const value = original.apply(this, arguments);
+      if (!document.contains(this)) {
+        document.body.append(this);
+      }
+      return value;
+    };
+  });
+})();
