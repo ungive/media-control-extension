3821c01aa0d146160798b88266912b920d1a2b80
Jonas van den Berg <jonas@vandenbe.com>
2024-10-30 20:57:24 +0100

    Fix required permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    permissions: ['storage'],
+    permissions: ['tabs']
   },
   imports: {
     addons: {
