a8fe5afb08d61dfacb8ca9ff0b170f701d25be25
Jonas van den Berg <jonas@vandenbe.com>
2024-10-30 01:23:35 +0100

    Fix typo

--- public/inject.js
+++ public/inject.js
@@ -1,7 +1,7 @@
 // Appends any audio elements that are being played or paused to the DOM
 // so that they can be found and used by the content script.
 // This hook needs to be executed within the web page, not a content script,
-// otherwise overwritin the Audio prototype has no effect.
+// otherwise overwriting the Audio prototype has no effect.
 
 (function hookFutureAudioElements() {
   ['play', 'pause'].forEach(method => {
