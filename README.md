5d659f76f83f4077adcc659cb264ae0d91660c98
Jonas van den Berg <jonas@vandenbe.com>
2026-05-07 16:44:40 +0000

    Add dev-container.sh

--- README.md
+++ README.md
@@ -33,12 +33,45 @@ You can also create a pop-out window to continuously observe media.
 
 ## Development
 
+### Quick start
+
 ```sh
 $ git clone https://github.com/ungive/media-control-extension
 $ cd media-control-extension
 $ pnpm install
 $ pnpm dev:firefox
 $ pnpn dev  # Chrome
+```
+
+### Developing inside a Docker container
+
+These steps are for [VSCodium](https://vscodium.com/) with the [Open Remote - SSH](https://open-vsx.org/vscode/item?itemName=jeanp413.open-remote-ssh) extension on Linux:
+
+```sh
+$ git clone https://github.com/ungive/media-control-extension
+$ cd media-control-extension
+$ bash ./dev-container.sh --start
+# Inside the container:
+% pnpm install
+% pnpm wxt --host 0.0.0.0 -b firefox  # Firefox
+% pnpm wxt --host 0.0.0.0 -b chrome  # Chrome
+```
+
+The first command creates and starts (or restarts) a dev container and opens VSCodium. After installing dependencies and starting WXT, open your host's browser and load the extension from the `.output` directory. If your browser is installed via Flatpak, you need to give the browser read/write permission to the `.output` directory. The extension should now auto-reload and you have a fully working development environment.
+
+If you want to make commits inside the container (run this outside the container):
+
+```sh
+$ git config --local user.name "$(git config --global --get user.name)"
+$ git config --local user.email "$(git config --global --get user.email)"
+```
+
+You'll have to push changes outside the container.
+
+To erase the container:
+
+```sh
+$ bash ./dev-container.sh --erase
 ```
 
 ## Copyright
---
+++ dev-container.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+set -euo pipefail
+PORT=2020
+KEY_PATH="$HOME/.ssh/dev-container"
+if [[ ! -f "$KEY_PATH" ]]; then
+    mkdir -p "$(dirname "$KEY_PATH")"
+    ssh-keygen -t ed25519 -f "$KEY_PATH" -N "" -q
+    echo "generated ssh key $KEY_PATH"
+else
+    echo "using ssh key $KEY_PATH"
+fi
+PUBKEY_PATH="$KEY_PATH.pub"
+PROJECT=$(basename $(pwd))
+CONTAINER="dev-container-$PROJECT"
+RECREATE=false
+ERASE=false
+STOP=false
+for arg in "$@"; do
+    if [[ "$arg" == "--recreate" ]]; then
+        RECREATE=true
+    fi
+    if [[ "$arg" == "--erase" ]]; then
+        ERASE=true
+    fi
+    if [[ "$arg" == "--stop" ]]; then
+        STOP=true
+    fi
+done
+if ! $RECREATE; then
+    if sudo docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER}$"; then
+        if $STOP; then
+            echo "stopping dev container"
+            sudo docker stop "$CONTAINER" 1>/dev/null || true
+            exit 0
+        fi
+        if $ERASE; then
+            echo "erasing dev container"
+            sudo docker stop "$CONTAINER" 1>/dev/null || true
+            sudo docker rm "$CONTAINER" 1>/dev/null || true
+            exit 0
+        fi
+        if sudo docker ps --format '{{.Names}}' | grep -q "^${CONTAINER}$"; then
+            echo "dev container is already running"
+	    codium --remote ssh-remote+localhost:$PORT /home/dev/work
+            exit 0
+        else
+            echo "dev container already exists, starting it again"
+            sudo docker start "$CONTAINER" 1>/dev/null
+	    codium --remote ssh-remote+localhost:$PORT /home/dev/work
+            exit 0
+        fi
+    fi
+    if $STOP || $ERASE; then
+        echo "dev container does not exist, done"
+        exit 0
+    fi
+fi
+export PUBKEY=$(cat "$PUBKEY_PATH")
+test -n "$PUBKEY" || exit 1
+CREATE_UID=$(id -u)
+CREATE_GID=$(id -g)
+sudo docker build -t "$CONTAINER" - <<EOF
+FROM node:24-slim
+USER root
+ENV DEBIAN_FRONTEND="noninteractive"
+RUN corepack enable && corepack prepare pnpm@latest --activate
+RUN apt-get update \
+	&& apt-get upgrade -y \
+	&& apt-get install -y software-properties-common \
+	&& apt-get install -y openssh-server wget git zsh
+RUN touch "/root/.hushlogin"
+RUN userdel -f node &&\
+	if getent group node; then groupdel node; fi &&\
+	groupadd devgroup -g ${CREATE_GID} &&\
+	useradd -l -s /usr/bin/zsh -u ${CREATE_UID} -g devgroup -m dev
+USER dev
+RUN touch "/home/dev/.hushlogin" &&\
+        echo "#" >> /home/dev/.zshrc
+VOLUME /home/dev/work
+USER root
+RUN mkdir /var/run/sshd
+EXPOSE 22
+CMD ["/usr/sbin/sshd", "-D"]
+EOF
+sudo docker stop "$CONTAINER" 1>/dev/null || true
+sudo docker rm "$CONTAINER" 1>/dev/null || true
+sudo docker run -d -it --name "$CONTAINER" --mount "type=bind,src=$PWD,dst=/home/dev/work" -p "$PORT:22" -p "3000:3000" --hostname dev "$CONTAINER"
+sudo docker exec -u dev "$CONTAINER" sh -c "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && echo '$PUBKEY' >> ~/.ssh/authorized_keys"
+codium --remote ssh-remote+localhost:$PORT /home/dev/work
