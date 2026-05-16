66f930218a5a2f7c330cc5f7b70d07ef36e79596
Jonas van den Berg <jonas@vandenbe.com>
2026-05-16 11:46:11 +0000

    Disable the context menu where appropriate

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -6,5 +6,12 @@ import './style.css';
 createApp(App).mount('#app');
 
 if (isPopout()) {
-  initPopout()
+  initPopout();
 }
+
+// Prevent showing the context menu for anything but link elements.
+document.body.addEventListener('contextmenu', (e) => {
+  if (e.target instanceof HTMLElement && e.target.closest('a') === null) {
+    e.preventDefault();
+  }
+});
