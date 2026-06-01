412adfc22ff8bd6dc174da54bbd33712c3ccd3c5
Jonas van den Berg <jonas@vandenbe.com>
2026-06-01 02:24:37 +0200

    Remove the old injection script

--- entrypoints/inject.content.ts
+++ entrypoints/inject.content.ts
@@ -1,3 +1,11 @@
+// Appends any audio elements that are being played or paused to the DOM so
+// that they can be found and used by the isolated content script. This hook
+// needs to be executed directly within the web page, i.e. in the main world,
+// otherwise overwriting the Audio prototype has no effect.
+
+// An example website that needs this injection is https://soundcloud.com.
+// Without it media wouldn't be detected immediately when it starts playing.
+
 function hookFutureAudioElements() {
   ['play', 'pause'].forEach(method => {
     const original = Audio.prototype[method];
