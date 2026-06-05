84d7311566fb112b6223ff402806d3d84dd15f4b
Jonas van den Berg <jonas@vandenbe.com>
2026-06-05 12:38:58 +0000

    Fix seeking sometimes not working

--- entrypoints/media.content.ts
+++ entrypoints/media.content.ts
@@ -42,10 +42,40 @@ function openTextLink(text: string) {
   }
 }
 
+// The period in milliseconds for which to retry seeking, so long as seeking
+// doesn't work on the first attempt or with any of the retry attempts.
+const SEEK_POSITION_RETRY_PERIOD = 2000; // 1000
+
+const SEEK_POSITION_VERIFY_PERIOD = 1000; // 250
+
+// The frequency at which to retry in milliseconds. Arbitrary, but high enough
+// for a 30 FPS video to hopefully not show the next frame of the old position.
+const SEEK_POSITION_RETRY_FREQUENCY = 1000 / 30; // 33.3ms
+
+const SEEK_POSITION_EPSILON_SECONDS = 500 / 1000;
+
+let seekPositionInterval: NodeJS.Timeout | null = null;
+let seekPositionTimestamp: number | null = null
+let seekPositionLastUpdateTimestamp: number | null = null
+let seekPositionLastMillis: number | null = null
+
 browser.runtime.onMessage.addListener((message: RuntimeMessage) => {
   if (!mediaObserver) {
     console.assert(false, 'Media observer not initialized');
     return;
+  }
+  // Clear any seek retry interval, if the message controls media playback. We
+  // don't want an old seek to interfere with new media control actions.
+  if (seekPositionInterval) {
+    switch (message.type) {
+      case PopupMessage.PauseMedia:
+      case PopupMessage.PlayMedia:
+      case PopupMessage.SeekStart:
+      case PopupMessage.SeekPosition:
+      case PopupMessage.NextTrack: {
+        clearInterval(seekPositionInterval);
+      }
+    }
   }
   switch (message.type) {
     case PopupMessage.PauseMedia: {
@@ -95,13 +125,61 @@ browser.runtime.onMessage.addListener((message: RuntimeMessage) => {
       const mediaElement = mediaObserver.mediaElement;
       const seekPositionPayload = message.payload as SeekPositionPayload;
       if (mediaElement !== null) {
+        const oldPosition = mediaElement.currentTime;
+        const newPosition = seekPositionPayload.position;
         if (mediaElement.paused) {
           mediaElement.play();
         }
         if (!mediaElement.paused) {
-          // If this ever does not work, we could use the current progress
-          // element and click on the respective position instead.
-          mediaElement.currentTime = seekPositionPayload.position;
+          // NOTE If this ever does not work, we could use the current progress
+          // element and click on the respective position in the DOM instead.
+          const seekFunction = function () {
+            const nowTimestamp = new Date().getTime();
+            const startTimeDelta = nowTimestamp - seekPositionTimestamp!;
+            const currentPosition = mediaElement.currentTime;
+            const oldPositionDelta = Math.abs(currentPosition - oldPosition);
+            const newPositionDelta = Math.abs(currentPosition - newPosition);
+            // Seeking is successful, when the seek position was updated at
+            // least once, when the adjusted current playback position is closer
+            // to the target position than to the old playback position and when
+            // the delta is less than the acceptable epsilon.
+            let done = false;
+            if (seekPositionLastUpdateTimestamp !== null &&
+                newPositionDelta < oldPositionDelta &&
+                newPositionDelta < SEEK_POSITION_EPSILON_SECONDS) {
+              const verifyDelta = nowTimestamp - seekPositionLastUpdateTimestamp;
+              if (verifyDelta < SEEK_POSITION_VERIFY_PERIOD) {
+                const currentPositionMillis = Math.floor(currentPosition * 1000);
+                // If seeking the playback position was successful and it is
+                // either at or advancing past the last seeked position, then we
+                // can stop verifying and retrying early, as the player is now
+                // very likely to keep playing from the target position.
+                if (seekPositionLastMillis !== null &&
+                    currentPositionMillis >= seekPositionLastMillis &&
+                    Math.abs(currentPositionMillis - seekPositionLastMillis) < SEEK_POSITION_EPSILON_SECONDS) {
+                  done = true;
+                } else {
+                  seekPositionLastMillis = currentPositionMillis;
+                  return;
+                }
+              } else {
+                done = true;
+              }
+            }
+            if (done || startTimeDelta > SEEK_POSITION_RETRY_PERIOD) {
+              clearInterval(seekPositionInterval!);
+              seekPositionInterval = null;
+              seekPositionTimestamp = null;
+              seekPositionLastUpdateTimestamp = null;
+              return;
+            }
+            mediaElement.currentTime = newPosition;
+            seekPositionLastUpdateTimestamp = new Date().getTime();
+          };
+          seekPositionTimestamp = new Date().getTime();
+          seekPositionInterval = setInterval(seekFunction, SEEK_POSITION_RETRY_FREQUENCY);
+          // A short timeout seems to be required for YouTube.
+          setTimeout(seekFunction, 0);
         }
       }
       break;
